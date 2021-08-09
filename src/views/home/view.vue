e9058100e3dc999b7ba98666e4a4e40884a17f59
Dmitry <pro100pro10010@gmail.com>
2021-08-09 16:34:15 +0300

    Use VSTP schema

---
+++ src/views/home/view.vue
@@ -0,0 +1,14 @@
+<script lang="ts" src="./index.ts"></script>
+<style lang="stylus" src="./style.styl"></style>
+
+<template lang="pug">
+.home
+  img(
+    src="../../assets/logo.png"
+    alt="Vue logo"
+    )
+
+  HelloWorld(
+    msg="Welcome to Your Vue.js + TypeScript App"
+    )
+</template>
